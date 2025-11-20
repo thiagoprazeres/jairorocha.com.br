6514031560af77f00affdc0e688ca4877251fe89
Thiago Prazeres <thiagoprazeres@gmail.com>
2025-11-20 12:45:18 -0300

    atualização de imóveis

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, {...config, providers: [provideZoneChangeDetection(), ...config.providers]}, context);
 
 export default bootstrap;
